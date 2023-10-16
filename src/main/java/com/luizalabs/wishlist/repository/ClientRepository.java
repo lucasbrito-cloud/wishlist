ad1db532d5c9cad5cb26c4205128de47eb4ad3d7
lucasbrito-cloud <84884596+lucasbrito-cloud@users.noreply.github.com>
2023-10-16 13:03:12 -0300

    Delete src directory

--- src/main/java/com/luizalabs/wishlist/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luizalabs.wishlist.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.luizalabs.wishlist.model.Client;
-
-public interface ClientRepository extends MongoRepository<Client, String> {
-    Client findByName(String name);
-}
--- src/main/java/com/luizalabs/wishlist/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luizalabs.wishlist.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.luizalabs.wishlist.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-    Product findByName(String name);
-}
